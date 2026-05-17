82a94a44f11f7375ba709ccb08d98c2ab65ebef1
jamesread <contact@jread.com>
2026-05-17 22:27:14 +0100

    fix: add frontend support

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,4 +2,14 @@
 
 set -e
 
-exec "/usr/local/bin/${ICEHIVE_SERVICE}" "$@"
+case "$ICEHIVE_SERVICE" in
+frontend)
+    FE_PORT=$(printf '%s' "${ICEHIVE_FRONTEND_PORT:-${PORT:-8080}}" | tr -cd '0-9')
+    : "${FE_PORT:=8080}"
+    sed "s/@LISTEN_PORT@/${FE_PORT}/g" /etc/nginx/icehive-frontend.conf.template >/tmp/icehive-frontend-nginx.conf
+    exec nginx -c /tmp/icehive-frontend-nginx.conf -g 'daemon off;'
+    ;;
+*)
+    exec "/usr/local/bin/${ICEHIVE_SERVICE}" "$@"
+    ;;
+esac
