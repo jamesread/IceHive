858227e8dae04c7d5d7c8d05f652b4c4460f0a9d
jamesread <contact@jread.com>
2026-05-23 23:33:00 +0100

    fix: frontend port parses correctly from kube

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,7 +4,15 @@ set -e
 
 case "$ICEHIVE_SERVICE" in
 frontend)
-    FE_PORT=$(printf '%s' "${ICEHIVE_FRONTEND_PORT:-${PORT:-8080}}" | tr -cd '0-9')
+    if [ -n "${ICEHIVE_FRONTEND_PORT:-}" ]; then
+        FE_PORT="${ICEHIVE_FRONTEND_PORT}"
+    elif [ -n "${PORT:-}" ]; then
+        # Kubernetes sets PORT to tcp://<ip>:<port>; take the port after the last colon.
+        FE_PORT="${PORT##*:}"
+    else
+        FE_PORT=8080
+    fi
+    FE_PORT=$(printf '%s' "$FE_PORT" | tr -cd '0-9')
     : "${FE_PORT:=8080}"
     sed "s/@LISTEN_PORT@/${FE_PORT}/g" /etc/nginx/icehive-frontend.conf.template >/tmp/icehive-frontend-nginx.conf
     exec nginx -c /tmp/icehive-frontend-nginx.conf -g 'daemon off;'
