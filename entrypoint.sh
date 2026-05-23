6fbf3214d1bc054384e546d1d305e848d06e8b4d
jamesread <contact@jread.com>
2026-05-24 00:06:49 +0100

    fix: print container version on startup, and fix frontend port

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,19 +1,33 @@
 #!/bin/sh
 
+printf '%s\n' "IceHive container version: ${ICEHIVE_VERSION:-dev}"
+
 set -e
+
+# Kubernetes may inject PORT (or a copied ICEHIVE_FRONTEND_PORT) as tcp://<ip>:<port>.
+parse_listen_port() {
+    val="$1"
+    case "$val" in
+    tcp://*)
+        val="${val##*:}"
+        ;;
+    esac
+    val=$(printf '%s' "$val" | tr -cd '0-9')
+    if [ -z "$val" ] || [ "$val" -lt 1 ] 2>/dev/null || [ "$val" -gt 65535 ] 2>/dev/null; then
+        val=8080
+    fi
+    printf '%s' "$val"
+}
 
 case "$ICEHIVE_SERVICE" in
 frontend)
     if [ -n "${ICEHIVE_FRONTEND_PORT:-}" ]; then
-        FE_PORT="${ICEHIVE_FRONTEND_PORT}"
+        FE_PORT=$(parse_listen_port "$ICEHIVE_FRONTEND_PORT")
     elif [ -n "${PORT:-}" ]; then
-        # Kubernetes sets PORT to tcp://<ip>:<port>; take the port after the last colon.
-        FE_PORT="${PORT##*:}"
+        FE_PORT=$(parse_listen_port "$PORT")
     else
         FE_PORT=8080
     fi
-    FE_PORT=$(printf '%s' "$FE_PORT" | tr -cd '0-9')
-    : "${FE_PORT:=8080}"
     sed "s/@LISTEN_PORT@/${FE_PORT}/g" /etc/nginx/icehive-frontend.conf.template >/tmp/icehive-frontend-nginx.conf
     exec nginx -c /tmp/icehive-frontend-nginx.conf -g 'daemon off;'
     ;;
